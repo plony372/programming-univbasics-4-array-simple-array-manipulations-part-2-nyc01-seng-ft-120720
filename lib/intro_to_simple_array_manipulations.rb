1d9cad8c9484766f2cab26ef9f51ac98845d6b7f
plony benav <menachemfuterfas@gmail.com>
2020-11-12 01:12:57 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,25 @@
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+
+def using_insert(array, new_element)
+  array.insert(4, new_element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+  
+  
+def using_flatten(array)
+array.flatten
+end
+
+def using_delete(array, s_tring)
+array.delete(s_tring)
+end
+
+def using_delete_at(array, i_nteger)
+  array.delete_at(i_nteger)
+end
